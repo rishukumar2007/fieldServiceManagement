b6130566f973b1c142dadc66bbcd1847b8c3b188
Rishu Kumar <rsu6089@gmail.com>
2026-08-13 11:30:57 +0530

    initial commit

---
+++ fieldServiceManagement/src/test/java/com/KEYSTONE/fieldServiceManagement/FieldServiceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KEYSTONE.fieldServiceManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FieldServiceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
